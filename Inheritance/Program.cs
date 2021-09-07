2b39c8aeccef2dfc3df4d28a7746081ace73588e
Demchenko Kirill <thegalaxydias@gmail.com>
2021-09-07 19:51:37 +0300

    Добавьте файлы проекта.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForumUser clark = new ForumUser("admin", "addmin", 999);
+            clark.CreateMessage();
+
+            //User carl = new User("customer", "customer", 555);
+            //carl.CreateMessage();
+        }
+    }
+
+    public class User
+    {
+        public string role;
+        public string login;
+        public int password;
+
+
+        public User(string r, string l, int p)
+        {
+            role = r;
+            login = l;
+            password = p;
+        }
+    }
+
+     public class ForumUser : User
+    {
+        public ForumUser (string r, string l, int p) : base ( r,  l, p)
+        {
+           
+        }
+        public void CreateMessage()
+        {
+            Console.WriteLine($"Role:  {role} \nLogin: {login} \nPassword: {password}");
+        }
+
+       
+    }
+ 
+}
+
+
